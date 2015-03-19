2c7ea8519f1ccbcf77266b5696b2050d73eafac3
Dave Shanklin <codedetail@gmail.com>
2015-03-18 15:36:58 -0900

    valgrind - memory leak fixes

--- MatchedRegion.cpp
+++ MatchedRegion.cpp
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <iostream>
 #include <math.h>
 #include "FireLog.h"
 #include "FireSight.hpp"
@@ -25,23 +26,25 @@ MatchedRegion::MatchedRegion(Range xRange, Range yRange, Point2f average, int po
 json_t *MatchedRegion::as_json_t() {
 	json_t *pObj = json_object();
 
-	json_object_set(pObj, "xmin", json_integer(xRange.start));
-	json_object_set(pObj, "xmax", json_integer(xRange.end));
-	json_object_set(pObj, "xavg", json_real(average.x));
-	json_object_set(pObj, "ymin", json_integer(yRange.start));
-	json_object_set(pObj, "ymax", json_integer(yRange.end));
-	json_object_set(pObj, "yavg", json_real(average.y));
-	json_object_set(pObj, "pts", json_integer(pointCount));
-	json_object_set(pObj, "ellipse", json_real(ellipse));
-	json_object_set(pObj, "covar", json_real(covar));
+	json_object_set_new(pObj, "xmin", json_integer(xRange.start));
+	json_object_set_new(pObj, "xmax", json_integer(xRange.end));
+	json_object_set_new(pObj, "xavg", json_real(average.x));
+	json_object_set_new(pObj, "ymin", json_integer(yRange.start));
+	json_object_set_new(pObj, "ymax", json_integer(yRange.end));
+	json_object_set_new(pObj, "yavg", json_real(average.y));
+	json_object_set_new(pObj, "pts", json_integer(pointCount));
+	json_object_set_new(pObj, "ellipse", json_real(ellipse));
+	json_object_set_new(pObj, "covar", json_real(covar));
 
 	return pObj;
 }
 
 string MatchedRegion::asJson() {
 	json_t *pObj = as_json_t();
-	char *pObjStr = json_dumps(pObj, JSON_PRESERVE_ORDER|JSON_COMPACT|JSON_INDENT(2));
-	string result(pObjStr);
+	std::string result;
+	char * char_result = json_dumps(pObj, JSON_PRESERVE_ORDER | JSON_COMPACT | JSON_INDENT(2));
+	result.assign(char_result);
+	free(char_result);
+	json_decref(pObj);
 	return result;
 }
-
